9b937028338aea4be474749a0bf1718450ab7f43
SDTTTTT <760159537@qq.com>
2020-03-10 20:03:10 +0800

    [SDTTTTT] Add Request Cache

--- src/request/bangumi.ts
+++ src/request/bangumi.ts
@@ -3,6 +3,7 @@ import Axios from "./instance";
 import { AxiosResponse } from "axios";
 import { BangumiUrl, BANGUMI_WEEK } from './bangumi_url';
 import { isEmptyArray, isEmptyObject } from '../utils/type';
+import { BangumiCache, WeekBangumiCache } from './cache';
 import {
     BangumisResponse,
     WeekBangumiData,
@@ -10,6 +11,8 @@ import {
     BangumisData,
     isSuccess
 } from './structure';
+
+
 
 /**
  * HTTP Request Gets all bangumi
@@ -22,21 +25,30 @@ export async function getAllBangumi(burl: BangumiUrl): Promise<BangumisData | un
 
     const url: string = burl.build().finalUrl;
 
-    const res: AxiosResponse<BangumisResponse> =
-        await Axios.get<any, AxiosResponse<BangumisResponse>>(url);
+    const cacheData: BangumisData | undefined = BangumiCache.get(url);
+    let result: BangumisData | undefined = cacheData;
 
-    const bangumisResponse: BangumisResponse = res.data;
-    isSuccess(bangumisResponse);
+    if (!cacheData) {
+        const res: AxiosResponse<BangumisResponse> =
+            await Axios.get<any, AxiosResponse<BangumisResponse>>(url);
 
-    if (isEmptyObject(bangumisResponse.data) || isEmptyArray(bangumisResponse.data.list)) {
-        vscode.window.showInformationMessage(`
-            获取数据为空🤔
-        `);
-        return;
+        const bangumisResponse: BangumisResponse = res.data;
+        isSuccess(bangumisResponse);
+
+        if (isEmptyObject(bangumisResponse.data) || isEmptyArray(bangumisResponse.data.list)) {
+            vscode.window.showInformationMessage(`
+                获取数据为空🤔
+            `);
+            return;
+        }
+        result = bangumisResponse.data;
+        BangumiCache.set(url, result);
     }
 
-    return bangumisResponse.data;
+    return result;
 }
+
+
 
 /**
  * Gets week bangumi
@@ -47,18 +59,28 @@ export async function getAllBangumi(burl: BangumiUrl): Promise<BangumisData | un
  */
 export async function getWeekBangumi(): Promise<Array<WeekBangumiData> | undefined> {
 
-    const res: AxiosResponse<WeekBangumiResponse> =
-        await Axios.get<any, AxiosResponse<WeekBangumiResponse>>(BANGUMI_WEEK);
+    const cacheData: Array<WeekBangumiData> | undefined =
+        WeekBangumiCache.get(BANGUMI_WEEK);
 
-    const weekBangumiResponse = res.data;
-    isSuccess(weekBangumiResponse);
+    let result: Array<WeekBangumiData> | undefined = cacheData;
 
-    if (isEmptyArray(weekBangumiResponse.result)) {
-        vscode.window.showInformationMessage(`
+    if (!cacheData) {
+        const res: AxiosResponse<WeekBangumiResponse> =
+            await Axios.get<any, AxiosResponse<WeekBangumiResponse>>(BANGUMI_WEEK);
+
+        const weekBangumiResponse = res.data;
+        isSuccess(weekBangumiResponse);
+
+        if (isEmptyArray(weekBangumiResponse.result)) {
+            vscode.window.showInformationMessage(`
             获取数据为空🤔
         `);
-        return;
+            return;
+        }
+        result = weekBangumiResponse.result;
+        WeekBangumiCache.set(BANGUMI_WEEK, result);
     }
 
-    return weekBangumiResponse.result;
+
+    return result;
 }
