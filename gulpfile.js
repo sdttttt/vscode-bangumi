73e586ed11a8ec2ebb09b103e2c0e546890ede03
sdttttt <sdttttt@outlook.com>
2020-12-03 15:50:35 +0800

    chore: Add task in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { src, dest } =  require("gulp");
+const { src, dest, watch, series } =  require("gulp");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProject } =  require( "gulp-typescript");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const imagemin = require("gulp-imagemin");
 
-async function defaultTask() {
+async function optimizeimgTask() {
+	src("resources/*")
+	.pipe(imagemin())
+	.pipe(dest("resources"));
+
+	src("Icon.png")
+	.pipe(imagemin())
+	.pipe(dest("."));
+}
+
+async function compileTask() {
 	const tsconfig = createProject("tsconfig.json");
 	const tsresult =  src("src/**/*.ts")
 	.pipe(tsconfig());
 
-	return tsresult.js.pipe(dest("out"))
+	return tsresult.js.pipe(dest("out"));
 }
 
-exports.default = defaultTask;
+exports.watchCompile = async () => {
+	watch("src/**/*.ts", compileTask);
+};
+
+exports.optimizeimg = optimizeimgTask
+exports.compile = compileTask
