d56d3ceb289601d58875762888436931292b2904
sdttttt <0wk3btfea@mozmail.com>
2023-03-27 16:34:26 +0800

    style: format.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     root   : true,
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:json/recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:json/recommended",
+    ],
     parser : "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     rules  : {
